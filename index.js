54e62f6b3e492f9be8c8b964c3891fd79a2ddd2f
Avery Brinkman <brinkmae@mail.uc.edu>
2021-06-10 17:32:18 -0400

    Basic routes

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import data from './data/data.json';
 
 const app = express();
 const PORT = 3000;
-// Created
+
+app.get('/', (req, res) => {
+    res.send(`a GET request with / route on port ${PORT}`);
+});
+
+app.post('/newItem', (req, res) => {
+    res.send(`a POST request with /newItem route on port ${PORT}`);
+});
+
+app.put('/item', (req, res) => {
+    res.send(`a PUT request with /item route on port ${PORT}`);
+});
+
+app.delete('/item', (req, res) => {
+    res.send(`a DELETE request with /item route on port ${PORT}`);
+});
 
 app.listen(PORT, () => {
-    console.info(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
+    console.log(data);
 });
