7df56e21c2a7a97874f1e5ac600bdd206faf1604
Avery Brinkman <brinkmae@mail.uc.edu>
2021-06-10 17:53:13 -0400

    Added public and changed image

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import data from './data/data.json';
 const app = express();
 const PORT = 3000;
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
     // get data first (Ex: from a MongoDB. Here it's from /data/data.json)
     res.json(data);
