410da0c381dae35aeb0a5a96aff0d1e2d789f317
Avery Brinkman <brinkmae@mail.uc.edu>
2021-06-10 17:57:44 -0400

    Static route for file serving

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ import data from './data/data.json';
 const app = express();
 const PORT = 3000;
 
+// This is for the public folder on path /
 app.use(express.static('public'));
+
+// This is for the images folder on path /images
+app.use('/images', express.static('images'));
 
 app.get('/', (req, res) => {
     // get data first (Ex: from a MongoDB. Here it's from /data/data.json)
